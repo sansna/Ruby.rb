0368b9dd6886ed626c61a19deb7ab5a0f04e4588
Sansna <sansna@localhost.local>
2019-10-27 15:48:48 +0800

    @Wentao[HEAD]: update first tutorial - sansna

---
+++ tutorial.rb
@@ -0,0 +1,51 @@
+# String manipulation
+#str = "Hello World"
+#puts str[0]
+#puts str[0,4]
+#puts str.index("W")
+#puts str.length
+
+# Removes newline of input
+#name = gets.chomp()
+#num = 10
+#puts ("hello " + num.to_s + " times " + name + " hi!")
+
+# gets always returns string. to_i, to_f converts it to numbers.
+# NOTE: non-numbers will be turned to 0.
+#age = gets.chomp()
+#puts (age.to_i*10)
+
+# arrays
+#friends = Array.new
+#friends[0] = "michael"
+#friends[5] = "james"
+# array output seperated by nl.
+#puts friends
+#puts friends.include?("michael")
+#puts friends.include?("andy")
+
+# Another array
+#another_friendlist = ["Hit", "hop", "dit"]
+#another_friendlist.delete("hop")
+#puts another_friendlist
+
+# KeyVal
+#states = {
+#  "Pennsylvania" => "PA",
+#  "New York" => "NY",
+#  "Oregon" => "OR",
+#  1 => "digit",
+#  :Var => "vars",
+#}
+#puts states["Oregon"]
+#puts states[1]
+#puts states[:Var]
+
+# Functions
+# can be without parenthesis, have default values.
+# also supports multiple values
+#def sayhi(name="Mike")
+#    puts "Hello " + name
+#    return 10, 20
+#end
+#puts sayhi[1]
