b9221114f14ec8789580d3051488d6a8f381509f
Sansna <sansna@localhost.local>
2019-10-27 23:36:09 +0800

    @Wentao[master]: update tutorial - sansna

--- tutorial.rb
+++ tutorial.rb
@@ -49,3 +49,81 @@
 #    return 10, 20
 #end
 #puts sayhi[1]
+
+# Conditions
+# not is same as !
+#ismale = false
+#if ismale
+#  puts "im male"
+#elsif not ismale
+#  puts "is female"
+#end
+
+# Switch case
+#day = "mon"
+#case day
+#when "mon"
+#  puts "Monday"
+#when "tue"
+#  puts "Tuesday"
+#else
+#  puts "Otherday"
+#end
+
+# While loop
+#i = 0
+#while i < 100
+#  i += 1
+#  puts i
+#end
+
+# Iteration over array
+#friends = ["keven", "karen", "oscar"]
+#for f in friends
+#  puts f
+#end
+
+# Another iteration
+#friends = ["keven", "karen", "oscar"]
+#friends.each do |f|
+#  puts f
+#end
+
+# For int block
+#for i in 0..100
+#  puts i
+#end
+#100.times do |i|
+#  puts i
+#end
+
+# Note the following begin/end forms a comment block
+=begin
+Comment line 1
+Comment line 2
+=end
+
+#File.open("./external.txt", "r") do |file|
+#  # file.read() is treated as string
+#  #puts file.read().include? "h2"
+#  
+#  #puts file.readline().include?"h2"
+#  #puts file.readline().include?"h2"
+#  
+#  # file.readchar() is also treated as string
+#  #puts file.readchar()
+#  #puts file.readchar().include?"1"
+#
+#  # file.readlines() is treated as []string
+#  #for l in file.readlines()
+#  #  puts l
+#  #end
+#end
+
+# file opened as variable
+#file = File.open(
+#  "./external.txt",
+#  "r"
+#)
+#puts file.read
+#file.close()
